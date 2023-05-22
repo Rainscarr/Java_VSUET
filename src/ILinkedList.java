951439ec815a4baf171dbc3ed74df7df722d42ae
Rainscarr <ww.ru13@mail.ru>
2023-05-22 12:52:47 +0300

    [add] files

---
+++ src/ILinkedList.java
@@ -0,0 +1,115 @@
+import java.util.function.Predicate;
+
+public class ILinkedList implements IList {
+    private Node head;
+    private int size;
+
+    private class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    @Override
+    public void add(int value) {
+        if (head == null) {
+            head = new Node(value);
+        } else {
+            Node newNode = new Node(value);
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    @Override
+    public void add(int index, int value) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (index == 0) {
+            Node newNode = new Node(value);
+            newNode.next = head;
+            head = newNode;
+        } else {
+            Node newNode = new Node(value);
+            Node current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+        size++;
+    }
+
+    @Override
+    public void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        if (index == 0) {
+            head = head.next;
+        } else {
+            Node current = head;
+            for (int i = 0; i < index - 1; i++) {
+                current = current.next;
+            }
+            current.next = current.next.next;
+        }
+        size--;
+    }
+
+    @Override
+    public void remove(Predicate<Integer> condition) {
+        Node current = head;
+        Node prev = null;
+
+        while (current != null) {
+            if (condition.test(current.value)) {
+                if (prev == null) {
+                    head = current.next;
+                } else {
+                    prev.next = current.next;
+                }
+                size--;
+            } else {
+                prev = current;
+            }
+            current = current.next;
+        }
+    }
+
+    @Override
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        Node current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.value;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node current = head;
+        while (current != null) {
+            sb.append(current.value).append(" ");
+            current = current.next;
+        }
+        return sb.toString();
+    }
+}
---
+++ src/IList.java
@@ -0,0 +1,13 @@
+import java.util.function.Predicate;
+
+public interface IList {
+    void add(int value);
+
+    void add(int index, int value);
+
+    void remove(int index);
+
+    void remove(Predicate<Integer> condition);
+
+    int get(int index);
+}
